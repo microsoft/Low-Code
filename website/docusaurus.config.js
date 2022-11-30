01b605e312b6ef76f81db5f1f464df246a817239
Nitya Narasimhan <n.narasimhan@gmail.com>
2022-11-30 13:14:47 -0500

    Initial Import - Docusaurus build + deploy action

---
+++ website/docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+
+  title: 'Low Code Empowered!',
+  tagline: 'Power Platform - Azure Cloud - Dev Tools',
+  url: 'https://microsoft.github.io',
+  baseUrl: '/Low-Code/',
+  favicon: 'img/favicon.ico',
+
+  // GitHub Pages Config
+  organizationName: 'microsoft', 
+  projectName: 'Low-Code', 
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+
+        blog: {
+          blogTitle: '#30DaysOfLowCode',
+          blogDescription: 'From Low Code to Fusion Development - empowered!',
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Recent Articles',
+          tagsBasePath: 'tags',
+          archiveBasePath: 'archive',
+          postsPerPage: 1,
+          showReadingTime: true,
+          sortPosts: 'descending'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+
+      navbar: {
+        title: 'Low Code',
+        logo: {
+          alt: 'Low Code Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/microsoft/Low-Code',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
